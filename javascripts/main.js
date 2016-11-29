6267ac5c9672bf4a09ea1535269982b8744fd776
Gaeun Kim <gaeunkim@ohs.stanford.edu>
2016-11-29 21:25:44 +0900

    Add Contributors Section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
 console.log('This would be the main JS file.');
+
+(function() {
+	$.ajax({
+		method: 'GET',
+		url: 'https://api.github.com/repos/fossasia/gci16.fossasia.org/contributors'
+	}).done(function(response) {
+		const $ul = $('#contributors-section > ul');
+
+		$.each(response, function(index, contributor) {
+			$ul.append("\
+				<li>\
+					<a target='_blank' href='https://github.com/" + contributor.login + "'>\
+						<img src='" + contributor.avatar_url + "' class='profile'>\
+						<div class='content'>\
+							<div class='name'>" + contributor.login + "</div>\
+							<div class='commit-count'>" + contributor.contributions + " commits </div>\
+						</div>\
+					</a>\
+				</li>"
+			);
+		});
+	});
+ })();
