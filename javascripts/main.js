0bfc21fa4d41a5343116bdf9afc3fca1da5b4f1e
Kaisar Arkhan <ykno@protonmail.com>
2016-12-01 16:53:00 +0700

    Add Linting CI
    Linter is a program that checks and solves for common code problems
    like bad pratices, inconsistency, style issues, high complexity,
    duplications, etc.
    
    It's a good idea to have linters as a test case for the continuous
    integration system.
    
    In this commit, I've fixed the reported linting issues.
    
    For HTMLLint, Optional Tag, Concerns Seperation, and Capitalization
    are disabled due to concerns of unexpected behaviors.
    
    While indentation check is disabled because the file uses 4 spaces
    rather than 2 spaces and SpaceConsistencyBear does indentation
    check out of the box.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,10 @@
 $(function(){
-
-	$.ajax({
-		url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/contributors"
-	}).done(function(data){
-		data.forEach(function(contributors){
-			$("#contributors-list").append("<div><img src="+contributors.avatar_url+"><a href="+contributors.html_url+"><i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i></a><p>"+contributors.login+"</p></div>");
-
-		});
-	});
-	
+    $.ajax({
+        url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/contributors"
+    }).done(function(data){
+        data.forEach(function(contributors){
+            // Ignore LineLengthBear
+            $("#contributors-list").append("<div><img src="+contributors.avatar_url+"><a href="+contributors.html_url+"><i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i></a><p>"+contributors.login+"</p></div>");
+        });
+    });
 });
