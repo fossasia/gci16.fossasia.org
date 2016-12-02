23f79871288d71d9ca2c770f3da9ad4f3f17b8d6
Oana Rosca <oanarosca1999@yahoo.com>
2016-12-02 20:09:07 +0200

    Improved contributors section

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,16 @@ $(function(){
     }).done(function(data){
         data.forEach(function(contributors){
             // Ignore LineLengthBear
-            $("#contributors-list").append("<div><img src="+contributors.avatar_url+"><a href="+contributors.html_url+"><i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i></a><p>"+contributors.login+"</p></div>");
+            var html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'><div class='avatar'>"
+            +"<img src="+contributors.avatar_url+"><div class='contribs'><p>"+contributors.contributions;
+            if (contributors.contributions === 1)
+            	html += " contribution";
+            else
+            	html += " contributions";
+            html += "</p></div><a href="+contributors.html_url+">"
+            +"<i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i><span>"+contributors.login+"</span>"
+            +"</a></div></div></div>";
+            $("#contributors-list").append(html);
         });
     });
 });
