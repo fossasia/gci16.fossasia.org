feb53fed2ea26cf58e57ff98bf87a3e4ec978ad1
Oana Rosca <oanarosca1999@yahoo.com>
2016-12-02 21:10:10 +0200

    Fixed inconsistencies

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,15 +4,19 @@ $(function(){
     }).done(function(data){
         data.forEach(function(contributors){
             // Ignore LineLengthBear
-            var html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'><div class='avatar'>"
-            +"<img src="+contributors.avatar_url+"><div class='contribs'><p>"+contributors.contributions;
-            if (contributors.contributions === 1)
-            	html += " contribution";
-            else
-            	html += " contributions";
-            html += "</p></div><a href="+contributors.html_url+">"
-            +"<i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i><span>"+contributors.login+"</span>"
-            +"</a></div></div></div>";
+            var html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'>";
+            html += "<div class='avatar'>";
+            html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
+            html += contributors.contributions;
+            if (contributors.contributions === 1) {
+                html += " contribution";
+            }
+            else {
+                html += " contributions";
+            }
+            html += "</p></div><a href="+contributors.html_url+">";
+            html += "<i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i><span>";
+            html += contributors.login+"</span></a></div></div></div>";
             $("#contributors-list").append(html);
         });
     });
