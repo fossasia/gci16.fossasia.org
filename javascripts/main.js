706ca09f7b0f4ba3012bcc3b09b1f506fce0bee1
Tarun Boddupalli <tarunbod@gmail.com>
2016-12-02 20:13:21 -0500

    started issues-section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,15 @@
+// var issueTemplate = '<div class="row"><div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="card"><h3>$TITLE</h3><h5>$BODY</h5><p><a href="$URL">Read More</a></p></div></div></div>';
+
+var issueTemplate = `
+<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+    <div class="issue-card">
+        <span class="issue-title">$TITLE</span>
+        <span class="issue-body">$BODY</span>
+        <span class="issue-link"><a href="$URL">Read More</a></span>
+    </div>
+</div>
+`;
+
 $(function(){
     $.ajax({
         url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/contributors"
@@ -20,4 +32,31 @@ $(function(){
             $("#contributors-list").append(html);
         });
     });
+
+
+    /* ISSUES LIST
+       Requirements:
+       - Only show open issues with the "feature"
+       - sort the list alphabetically
+       - include a "read more" link that opens the issue page
+     */
+    $.ajax({
+        url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/issues",
+        data: {
+            state: "open",
+            // labels: "feature"
+        }
+    }).done(function(response) {
+        var finalHTML = "";
+        response.forEach(function(issue) {
+            var html = issueTemplate
+                .replace('$TITLE', issue.title.substring(0, 15) + '...')
+                .replace('$BODY', issue.body.length > 0 ? issue.body.substring(0, 200) + '...' : '')
+                .replace('$URL', issue.url);
+            finalHTML += html;
+        });
+        if (finalHTML.length > 0) {
+            $('#issues-list').html(finalHTML);
+        }
+    })
 });
