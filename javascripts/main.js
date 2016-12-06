eebaf981fa2e1d8882dc6b6a556da516cef62895
Wermarter <wermarter@gmail.com>
2016-12-06 13:44:24 +0700

    Added Chien to students section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,33 +1,47 @@
-$(function(){
+$(function () {
     $.ajax({
         url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/contributors"
-    }).done(function(data){
-        data.forEach(function(contributors){
+    }).done(function (data) {
+        data.forEach(function (contributors) {
             // Ignore LineLengthBear
             var html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'>";
             html += "<div class='avatar'>";
-            html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
+            html += "<img src=" + contributors.avatar_url + "><div class='contribs'><p>";
             html += contributors.contributions;
             if (contributors.contributions === 1) {
                 html += " contribution";
-            }
-            else {
+            } else {
                 html += " contributions";
             }
-            html += "</p></div><a href="+contributors.html_url+">";
+            html += "</p></div><a href=" + contributors.html_url + ">";
             html += "<i class='fa fa-github fa-2x gh-icon' aria-hidden='true'></i><span>";
-            html += contributors.login+"</span></a></div></div></div>";
+            html += contributors.login + "</span></a></div></div></div>";
             $("#contributors-list").append(html);
         });
     });
 });
 
-$(function(){
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(function () {
     $.ajax({
         url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/issues?state=open"
-    }).done(function(data){
+    }).done(function (data) {
         var index = 0;
-        data.forEach(function(issue){
+        data.forEach(function (issue) {
             if (index > 9) { // show the 10 latest issues
                 return false;
             }
@@ -35,14 +49,14 @@ $(function(){
                 return;
             }
             index++;
-            var html = "<div class='issue'><span>#"+issue.number+"</span>";
-            html += "<a href='"+issue.html_url+"' target='_blank'>"+issue.title+"</a>";
-            html += "<p>opened by </p>";
-            html += "<a href='"+issue.user.html_url+"' target='_blank' class='user'>";
-            html += issue.user.login+"</a><div class='right-coms'>";
-            html += "<a href='"+issue.html_url+"' class='comments' target='_blank'>";
-            html += "<i class='fa fa-comment'></i>"+issue.comments;
-            html += "</a></div></div>";
+            var html = "<div class='issue'><span>#" + issue.number + "</span>";
+            html += "<a href='" + issue.html_url + "' target='_blank'>" + issue.title + "</a>";
+            html += "<p>Opened by</p>";
+            html += "<a href='" + issue.user.html_url + "' target='_blank' class='user'>";
+            html += issue.user.login + "</a><div class='right-coms'>";
+            html += "<a href='" + issue.html_url + "' class='comments' target='_blank'>";
+            html += "<i class='fa fa-comment'></i>" + issue.comments;
+            html += "</a></div></a></div>";
             $(".issues-wrapper").append(html);
         });
     });
