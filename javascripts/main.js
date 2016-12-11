0872b44a36506ee7a10da7df695220cf0c3c5db8
kaine119 <muikaien1@gmail.com>
2016-12-12 00:27:42 +0800

    Add categorisation to "Latest Issues" section (#444)
    * Add categorisation to "Latest Issues" section
    
    Issues are now categorised by label assigned. If the issue has the label, it's in the category.
    
    * Fix travisCI build
    
    * Fix more travis issues
    
    * More travisCI fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -293,25 +293,66 @@ $(function() {
   $.ajax({
     url: "https://api.github.com/repos/fossasia/gci16.fossasia.org/issues?state=open"
   }).done(function(data) {
-    var index = 0;
-    data.forEach(function(issue) {
-      if (index > 9) { // show the 10 latest issues
-        return false;
+    var labels = [];
+    var labelNames = [];
+    var mainWrapper = $("#issues-categories-wrapper");
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].hasOwnProperty("pull_request") || data[i].closed_at !== null) {
+        data.splice(i, 1);
+        i--;
+        continue;
       }
-      if (issue.hasOwnProperty("pull_request")) {
-        return;
+    }
+    for (i = 0; i < data.length; i++) {
+      if (data[i].labels.length === 0) {
+        var issueElement = $('<div class="issue"></div>')
+                .append($("<span></span>").append(data[i].number))
+                .append($("<a></a>").attr("target", "_blank").attr("href", data[i].html_url).append(data[i].title))
+                .append($("<p>Opened by </p>").append($("<a></a>").append(data[i].user.login).attr("href", data[i].user.html_url).attr('target', '_blank')))
+                .append($('<div class="right-coms"></div>')
+                  .append($("<a class='comments'></a>")
+                    .attr("href", data[i].html_url)
+                    .attr('target', '_blank')
+                    .append($("<i class='fa fa-comment'></i>"))
+                    .append(data[i].comments))
+                );
+        issueElement.appendTo($("#unlabeled-category"));
       }
-      index++;
-      var html = "<div class='issue'><span>#" + issue.number + "</span>";
-      html += "<a href='" + issue.html_url + "' target='_blank'>" + issue.title + "</a>";
-      html += "<p>Opened by</p>";
-      html += "<a href='" + issue.user.html_url + "' target='_blank' class='user'>";
-      html += issue.user.login + "</a><div class='right-coms'>";
-      html += "<a href='" + issue.html_url + "' class='comments' target='_blank'>";
-      html += "<i class='fa fa-comment'></i>" + issue.comments;
-      html += "</a></div></a></div>";
-      $(".issues-wrapper").append(html);
-    });
+      for (var j = 0; j < data[i].labels.length; j++) {
+        if ($.inArray(data[i].labels[j].name, labelNames) === -1) {
+          labelNames.push(data[i].labels[j].name);
+          labels.push(data[i].labels[j]);
+        }
+      }
+    }
+    for (var i = 0; i < labels.length; i++) {
+      var categoryElement = $('<div class="issues-category"></div>');
+      var titleButton = $('<div class="button"></div>')
+          .append($('<a></a>').append(labels[i].name).attr("href", labels[i].html_url))
+          .css("background", "#" + labels[i].color);
+      categoryElement.append(titleButton);
+      for (j = data.length - 1; j >= 0; j--) {
+        for (var k = data[j].labels.length - 1; k >= 0; k--) {
+          if (data[j].labels[k].name === labels[i].name) {
+            // all hail .append()
+            // build the issue element
+            issueElement = $('<div class="issue"></div>')
+                .append($("<span></span>").append(data[j].number))
+                .append($("<a></a>").attr("target", "_blank").attr('href', data[j].html_url).append(data[j].title))
+                .append($("<p>Opened by </p>").append($("<a></a>").append(data[j].user.login).attr("href", data[j].user.html_url).attr('target', '_blank')))
+                .append($('<div class="right-coms"></div>')
+                  .append($("<a class='comments'></a>")
+                    .attr("href", data[j].html_url)
+                    .attr('target', '_blank')
+                    .append($("<i class='fa fa-comment'></i>"))
+                    .append(data[j].comments))
+                );
+            categoryElement.append(issueElement);
+            }
+        }
+      }
+      mainWrapper.append(categoryElement);
+    }
   });
 });
 $(document).ready(function() {
