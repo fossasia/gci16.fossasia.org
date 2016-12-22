7b960b6818bfd052db254f9971ff4b5995ca4a32
Robert O'Connor <rob@oconnor.ninja>
2016-12-21 20:10:31 -0500

    Make the FIX_BRANCH variable isolated to the gh-pages branch check

--- scripts/coala_patch.sh
+++ scripts/coala_patch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Push Coala fixes back to the repo if on gh-pages branch
-# make a branch per fix
-FIX_BRANCH="coala-fix-$TRAVIS_BUILD_NUMBER"
 # only do this on gh-page branch
 if [ "$TRAVIS_BRANCH" = "gh-pages" ]; then
+    # make a branch per fix
+    FIX_BRANCH="coala-fix-$TRAVIS_BUILD_NUMBER"
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "coala-autofix-bot"
     git checkout -B "$FIX_BRANCH"
